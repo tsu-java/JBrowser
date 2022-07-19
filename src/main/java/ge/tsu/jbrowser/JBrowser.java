05cf68f47330c4f97d61a12874ea80a6d2952e68
Vakhtang Laluashvili <v.laluashvili@sda.gov.ge>
2022-07-19 17:34:59 +0400

    Fix new tab buttom. Add empty style.css file

--- src/main/java/ge/tsu/jbrowser/JBrowser.java
+++ src/main/java/ge/tsu/jbrowser/JBrowser.java
@@ -12,10 +12,20 @@ public class JBrowser extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(JBrowser.class.getResource("main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Scene mainScene = new Scene(fxmlLoader.load(), 800, 600);
+        addStyle(mainScene);
         stage.setTitle("JBrowser");
-        stage.setScene(scene);
+        stage.setScene(mainScene);
         stage.show();
+    }
+
+    /**
+     * We add style.css here and also in main.fxml file for development purposes.
+     *
+     * @param scene
+     */
+    private void addStyle(Scene scene) {
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
     }
 
     public static void main(String[] args) {
--- src/main/java/ge/tsu/jbrowser/MainController.java
+++ src/main/java/ge/tsu/jbrowser/MainController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
 import java.net.URL;
